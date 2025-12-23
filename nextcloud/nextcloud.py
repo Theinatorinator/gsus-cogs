b517133fde6afcb3634516495aa27b2f476951f9
AA <aradia.gsus@gmail.com>
2025-12-22 19:06:07 -0800

    Add user_info endpoint

--- nextcloud/nextcloud.py
+++ nextcloud/nextcloud.py
@@ -3,6 +3,8 @@ from io import BytesIO
 from tsutils.cogs.apicog import CogWithEndpoints, endpoint
 
 class NextCloud(CogWithEndpoints):
+    GSUS_SERVER_ID = 1054471846436798535
+
     """A cog to interface with NextCloud."""
     def __init__(self, bot, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -20,9 +22,25 @@ class NextCloud(CogWithEndpoints):
         """
         return
 
-    @endpoint("hello")
-    async def hello_world(self, name):
-        """Adds two numbers."""
-        return f"Hello, {name}!"
+    @endpoint("user_info")
+    async def user_info(self, user_id: int):
+        """Get info about a user."""
+        member = await self.bot.get_guild(self.GSUS_SERVER_ID).fetch_member(user_id)
+        if member is None:
+            return {
+                'response': {'error': 'Member not in GSUS server.'},
+                'status': 404
+            }
+
+        return {
+                'response': {
+                    'avatar_url': member.avatar.url if member.avatar else None,
+                    'name': member.name,
+                    'account_creation': member.created_at.timestamp(),
+                    'roles': [role.id for role in member.roles],
+                },
+                'status': 404
+            }
+
 
 
